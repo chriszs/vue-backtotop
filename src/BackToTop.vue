a46b7e6dc20ac501e50941abecc1a0d81ab4cebb
Chris Zubak-Skees <chriszs@gmail.com>
2018-03-07 11:23:07 -0500

    Update BackToTop.vue

--- src/BackToTop.vue
+++ src/BackToTop.vue
@@ -44,6 +44,10 @@ export default {
    * @return {void}
    */
   created () {
+    if (typeof window === 'undefined' || !window) {
+      return false;
+    }
+  
     let catchScroll = () => {
       this.visible = (window.pageYOffset > parseInt(this.visibleOffset))
     }
